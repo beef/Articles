6002876cea4a4fe7ab8ea6cd0b05e2e7af9b9cf6
Steve England <steve@wearebeef.co.uk>
2009-06-23 16:08:22 +0100

    Correcting module code

--- lib/articles.rb
+++ lib/articles.rb
@@ -1,5 +1,7 @@
-module Beef::Articles
-  def article_permalink(article, options = {})
-    article_permalink_path(article.published_at.year,article.published_at.month,article.published_at.day,article.permalink,options)
+module Beef
+  module Articles
+    def article_permalink(article, options = {})
+      article_permalink_path(article.published_at.year,article.published_at.month,article.published_at.day,article.permalink,options)
+    end
   end
 end
