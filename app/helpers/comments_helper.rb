07b1878fccd0b4e929494af1ede216ca0196500f
Steve England <steve@wearebeef.co.uk>
2009-09-09 13:26:34 +0100

    Fixing need for commomentable in partial

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
 
   def comments_list(commentable)
-    content_tag :ul, render(:partial => commentable.comments, :locals => { :commentable => commentable }), :id => 'comment-list'  unless commentable.comments.empty?
+    content_tag :ul, render(:partial => commentable.comments), :id => 'comment-list' unless commentable.comments.empty?
   end
 
 end
