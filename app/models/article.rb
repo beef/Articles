09c1229b205e0e01a60c769199a54170959fa78b
Steve England <steve@wearebeef.co.uk>
2010-07-02 13:13:04 +0100

    Remove need for acts_as_textile

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,18 +1,18 @@
 class Article < ActiveRecord::Base
   belongs_to :category
-  
+
   acts_as_content_node
   acts_as_commentable
   has_assets
-  acts_as_textiled :body
+  acts_as_textiled :body if respond_to?(:acts_as_textiled)
   acts_as_taggable_on :tags
-  
+
   named_scope :categorised, lambda { |category|
     return {} if category.nil?
     category = Category.find(category) unless category.nil? or category.is_a? Category
     { :conditions => { :category_id => category.id }  }
   }
-  
+
   named_scope :in_time_delta, lambda { |year, month, day|
     return {} if year.blank?
     from = Time.mktime(year, month || 1, day || 1)
@@ -22,15 +22,15 @@ class Article < ActiveRecord::Base
     to   = to.tomorrow    unless month.blank?
     { :conditions => ['published_at BETWEEN ? AND ?', from, to ] }
   }
-  
+
   default_scope :order => 'published_at DESC'
-  
+
   validates_presence_of :body, :description, :tag_list, :if => :publish
-  
+
   # Default per page options
   cattr_accessor :per_page
   @@per_page = 10
-  
+
   # Finds one article which was posted on a certain date and matches the supplied dashed-title
   def self.find_by_permalink(year, month, day, permalink)
     if result = in_time_delta(year, month, day).first( :conditions => ["permalink = ?", permalink] )
@@ -39,5 +39,5 @@ class Article < ActiveRecord::Base
       raise ActiveRecord::RecordNotFound, "Couldn't find article with permalink #{permalink} on #{year}.#{month}.#{day}}"
     end
   end
-  
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
 
-  acts_as_textiled :body => [:hard_breaks, :filter_html, :filter_styles]
+  acts_as_textiled :body => [:hard_breaks, :filter_html, :filter_styles] if respond_to?(:acts_as_textiled)
 
   validates_presence_of :name, :comment
   validates_acceptance_of :spam_filter, :accept => true, :on => :create, :allow_nil => false, :message => "believes message to be spam."
